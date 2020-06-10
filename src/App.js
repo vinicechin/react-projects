b6c1a22053789564153587ad380bf0ae46eef1e5
Vinicius Cechin <vinicechin@gmail.com>
2020-06-10 11:10:29 +0200

    Add exercises controller to App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Exercise1 from './Exercises/Exercise1/Exercise1'
+
 import './App.css';
 
 class App extends Component {
+  isExercise = true
+
+  state = {
+    exercise: "1"
+  }
+
+  changeExercise = (event) => {
+    this.setState({
+      exercise: event.target.value
+    })
+  }
+
+  renderSwitch(exercise) {
+    switch(exercise) {
+      case "1":
+        return (
+          <Exercise1 />
+        )
+      default:
+        return null
+    }
+  }
+
+  renderClass() {
+    return <div>class</div>
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          this.isExercise &&
+          <select name="exercises" id="ex-select" onChange={this.changeExercise}>
+            <option value="1">Exercise 1</option>
+          </select>
+        }
+        { this.isExercise && this.renderSwitch(this.state.exercise) }
+        { !this.isExercise && this.renderClass() }
       </div>
     );
   }
---
+++ src/Exercises/Exercise1/Exercise1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Exercise1 extends Component {
+    render() {
+        return (
+            <div>
+                teste exercise 1
+            </div>
+        )
+    }
+}
+
+export default Exercise1
