ba5242cc8fe15e6efcb6782cecef0afd976e765a
Vinicius Cechin <vinicechin@gmail.com>
2020-06-15 10:42:35 +0200

    Change radium library for styled-components. Change necesssary code to keep working.

--- src/Sections/Person/Person.js
+++ src/Sections/Person/Person.js
@@ -1,22 +1,30 @@
 import React from 'react'
-import Radium from 'radium'
+import styled from 'styled-components'
 
-import "./Person.css"
+// import "./Person.css"
+
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+
+    @media (min-width: 650px) {
+        width: 390px;
+    }
+`
 
 const person = (props) => {
-    const style = {
-        '@media (min-width: 650px)': {
-            width: '390px'
-        }
-    }
-
     return (
-        <div className="Person" style={style}>
+        // <div className="Person" style={style}>
+        <StyledDiv>
             <p onClick={props.click}>I'm a { props.name }! and i am { props.age } years old</p>
             <p>{ props.children }</p>
             { props.changed && <input type="text" value={props.name} onChange={props.changed} /> }
-        </div>
+        </StyledDiv>
     )
 }
 
-export default Radium(person)
+export default person
