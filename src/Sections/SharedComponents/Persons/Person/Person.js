fd314426937c23e25bf1a60c19bb8712587ed3a0
Vinicius Cechin <vinicechin@gmail.com>
2020-06-17 11:22:44 +0200

    Add examples of how to wrap components to return without needing a div (or any similar) component

--- src/Sections/SharedComponents/Persons/Person/Person.js
+++ src/Sections/SharedComponents/Persons/Person/Person.js
@@ -3,13 +3,23 @@ import React from 'react'
 import styles from "./Person.module.css"
 
 const person = (props) => {
-    return (
-        <div className={styles.Person}>
-            <p onClick={props.click}>I'm a { props.name }! and i am { props.age } years old</p>
-            <p>{ props.children }</p>
-            { props.changed && <input type="text" value={props.name} onChange={props.changed} /> }
-        </div>
-    )
+    // return [
+    //     <p key="e1" onClick={props.click}>I'm a { props.name }! and i am { props.age } years old</p>,
+    //     <p key="e2">{ props.children }</p>,
+    //     <input key="e3" type="text" value={props.name} onChange={props.changed} />
+    // ]
+
+    // return <>
+    //     <p onClick={props.click}>I'm a { props.name }! and i am { props.age } years old</p>
+    //     <p>{ props.children }</p>
+    //     <input type="text" value={props.name} onChange={props.changed} />
+    // </>
+
+    return <div className={styles.Person}>
+        <p onClick={props.click}>I'm a { props.name }! and i am { props.age } years old</p>
+        <p>{ props.children }</p>
+        <input type="text" value={props.name} onChange={props.changed} />
+    </div>
 }
 
 export default person
