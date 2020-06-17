f80bc1132857306d08631b7fc6b1440c1eaf5fb3
Vinicius Cechin <vinicechin@gmail.com>
2020-06-17 15:57:21 +0200

    Send new post info throught post message

--- src/Sections/ReachingWeb/Blog/NewPost/NewPost.js
+++ src/Sections/ReachingWeb/Blog/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './NewPost.css';
 
@@ -6,7 +7,19 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Vini'
+    }
+
+    postDataHandler = () => {
+        const post = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        }
+        axios.post('https://jsonplaceholder.typicode.com/posts', post)
+            .then(response => {
+                console.log(response)
+            })
     }
 
     render () {
@@ -19,10 +32,10 @@ class NewPost extends Component {
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                    <option value="Vini">Vini</option>
+                    <option value="Gabi">Gabi</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={ this.postDataHandler }>Add Post</button>
             </div>
         );
     }
