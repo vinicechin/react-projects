8a86206461b281cabc8faef8bf56db76da2a21b7
Vinicius Cechin <vinicechin@gmail.com>
2020-06-17 16:01:39 +0200

    Add delete post behavior

--- src/Sections/ReachingWeb/Blog/FullPost/FullPost.js
+++ src/Sections/ReachingWeb/Blog/FullPost/FullPost.js
@@ -17,6 +17,13 @@ class FullPost extends Component {
         }
     }
 
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                console.log(response)
+            })
+    }
+
     render () {
         let post = <p style={{ textAlign: 'center' }} >Please select a Post!</p>
         if (this.props.id) {
@@ -28,7 +35,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button className="Delete" onClick={ this.deletePostHandler } >Delete</button>
                     </div>
                 </div>
     
