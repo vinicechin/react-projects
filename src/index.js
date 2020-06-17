5c9a0030fe6086daf54ee7129fb6bf9a3141486c
Vinicius Cechin <vinicechin@gmail.com>
2020-06-17 16:15:33 +0200

    Use interceptors to edit requests and responses behavior

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios'
+
+// configure before sending and catch sending errors
+const requestInterceptor = axios.interceptors.request.use(config => {
+    // edit request, like auth
+    return config
+}, error => {
+    console.log(error)
+    return Promise.reject(error)
+})
+
+// modify response before using it and catch response errors
+const responseInterceptor = axios.interceptors.response.use(response => {
+    // edit response
+    return response
+}, error => {
+    console.log(error)
+    return Promise.reject(error)
+})
+
+// remove interceptors (added only as example)
+axios.interceptors.request.eject(requestInterceptor)
+axios.interceptors.response.eject(responseInterceptor)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
