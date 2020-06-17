1666ba1c4a58ce38119fc6d60942946980eb8181
Vinicius Cechin <vinicechin@gmail.com>
2020-06-17 20:54:59 +0200

    Add switch to control routes

--- src/Sections/shared/components/Blog/Blog.js
+++ src/Sections/shared/components/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom'
+import { Route, NavLink, Switch } from 'react-router-dom'
 
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
@@ -28,9 +28,12 @@ class Blog extends Component {
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={() => <Posts />} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                {/* switch -> route to first route found */}
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
