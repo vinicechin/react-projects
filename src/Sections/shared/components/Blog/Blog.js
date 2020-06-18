920eff42fdb8d6234cbf461f9c3a231984b2c543
Vinicius Cechin <vinicechin@gmail.com>
2020-06-18 10:11:53 +0200

    Add guard to new post route

--- src/Sections/shared/components/Blog/Blog.js
+++ src/Sections/shared/components/Blog/Blog.js
@@ -6,6 +6,10 @@ import NewPost from './NewPost/NewPost'
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        auth: false
+    }
+
     render () {
         return (
             <div className="Blog">
@@ -30,7 +34,7 @@ class Blog extends Component {
                 {/* switch -> route to first route found */}
                 <Switch>
                     <Route path="/posts" component={Posts} />
-                    <Route path="/new-post" component={NewPost} />
+                    { this.state.auth && <Route path="/new-post" component={NewPost} /> }
                     <Redirect from="/" to="/posts" />
                 </Switch>
             </div>
