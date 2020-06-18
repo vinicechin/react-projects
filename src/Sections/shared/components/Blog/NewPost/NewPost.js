3da0f241b20d52f54d5060521eb04a4312f42297
Vinicius Cechin <vinicechin@gmail.com>
2020-06-18 10:01:07 +0200

    Add conditional redirect

--- src/Sections/shared/components/Blog/NewPost/NewPost.js
+++ src/Sections/shared/components/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 
 import './NewPost.css';
 
@@ -7,7 +8,8 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Vini'
+        author: 'Vini',
+        submitted: false
     }
 
     postDataHandler = () => {
@@ -19,12 +21,14 @@ class NewPost extends Component {
         axios.post('/posts', post)
             .then(response => {
                 console.log(response)
+                this.setState({ submitted: true })
             })
     }
 
     render () {
         return (
             <div className="NewPost">
+                {this.state.submitted && <Redirect to="/posts" />}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
