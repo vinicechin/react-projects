120b680e9eba2cc8a2a3df35e067f20502a8cd66
Vinicius Cechin <vinicechin@gmail.com>
2020-06-26 12:15:11 +0200

    Add state to BurgerBuilder. Dinamically build burger ingredients.

--- src/Project/components/Burger/Burger.js
+++ src/Project/components/Burger/Burger.js
@@ -4,11 +4,16 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import classes from './Burger.module.css';
 
 const Burger = (props) => {
+    const ingredients = Object.keys(props.ingredients).map((ingredient) => {
+        return [...Array(props.ingredients[ingredient])].map((_, index) => {
+            return <BurgerIngredient key={ingredient + index} type={ingredient} />
+        })
+    })
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            { ingredients }
             <BurgerIngredient type="bread-bottom" />
         </div>
     )
