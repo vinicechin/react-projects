0f20f04e0bd535c87af64494c2e2283324f1125c
Vinicius Cechin <vinicechin@gmail.com>
2020-06-26 12:01:27 +0200

    Implement Burger class and link with BurgerBuilder

--- src/Project/containers/BurgerBuilder/BurgerBuilder.js
+++ src/Project/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
+import Burger from '../../components/Burger/Burger'
+
 class BurgerBuilder extends Component {
     render() {
         return (
             <>
-                <div>Burger</div>
+                <Burger />
                 <div>Build Controls</div>
             </>
         )
