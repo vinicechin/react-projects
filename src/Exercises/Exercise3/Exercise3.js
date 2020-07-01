ee5671ee0dc101a137015bba03508b712c3333f2
Vinicius Cechin <vinicechin@gmail.com>
2020-07-01 13:46:54 +0200

    Add 404 route and redirect from all-courses to courses

--- src/Exercises/Exercise3/Exercise3.js
+++ src/Exercises/Exercise3/Exercise3.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Switch, Route, withRouter, NavLink } from 'react-router-dom'
+import { Switch, Route, withRouter, NavLink, Redirect } from 'react-router-dom'
 
 import Courses from './containers/Courses/Courses'
 import Users from './containers/Users/Users'
@@ -47,8 +47,10 @@ class Exercise3 extends Component {
       <div className={classes.Exercise3}>
         {this.renderNavBar()}
         <Switch>
-          <Route path={this.props.match.url + '/users'} component={ROUTES['users'].component} />
-          <Route path={this.props.match.url + '/courses'} component={ROUTES['courses'].component} />
+          <Route path={`${this.props.match.url}/users`} component={ROUTES['users'].component} />
+          <Route path={`${this.props.match.url}/courses`} component={ROUTES['courses'].component} />
+          <Redirect from={`${this.props.match.url}/all-courses`} to={`${this.props.match.url}/courses`} />
+          <Route render={() => <p>404 - Page not Found</p>} />
         </Switch>
       </div>
     )
@@ -56,14 +58,3 @@ class Exercise3 extends Component {
 }
 
 export default withRouter(Exercise3)
-
-/* <ol style={{textAlign: 'left'}}>
-  +<li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
-  +<li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
-  +<li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
-  <li>Pass the course ID to the "Course" page and output it there</li>
-  <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
-  <li>Load the "Course" component as a nested component of "Courses"</li>
-  <li>Add a 404 error page and render it for any unknown routes</li>
-  <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
-</ol> */
