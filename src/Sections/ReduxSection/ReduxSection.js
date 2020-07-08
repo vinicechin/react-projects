5ec445500b3f675267b6c61fcfaa0309a02178ec
Vinicius Cechin <vinicechin@gmail.com>
2020-07-08 11:52:35 +0200

    Add middleware to log actions sent to redux and old/new states

--- src/Sections/ReduxSection/ReduxSection.js
+++ src/Sections/ReduxSection/ReduxSection.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 
 import Counter from '../shared/components/Counter/Counter'
 import reducer from '../shared/store/reducer'
 
-const store = createStore(reducer)
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Log] old state', store.getState())
+            console.log('[Log] Dispatching', action)
+            const result = next(action)
+            console.log('[Log] next state', store.getState())
+            return result
+        }
+    }
+}
+
+const store = createStore(reducer, applyMiddleware(logger))
 
 const ReduxSection = () => {
     return (
