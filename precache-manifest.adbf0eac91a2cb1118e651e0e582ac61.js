33847428d8bc7c2524699ceff4bad6f512c24e0a
Vinicius Cechin <vinicechin@gmail.com>
2020-07-20 10:49:09 +0200

    Updates

---
+++ precache-manifest.adbf0eac91a2cb1118e651e0e582ac61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9f42a0f60ca241b78eb6186aca8b48",
+    "url": "/react-projects/index.html"
+  },
+  {
+    "revision": "06a844102e6bbcfffefb",
+    "url": "/react-projects/static/css/main.9a52e8e6.chunk.css"
+  },
+  {
+    "revision": "3af6fa693da3b17327e3",
+    "url": "/react-projects/static/js/2.26fe6def.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-projects/static/js/2.26fe6def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a844102e6bbcfffefb",
+    "url": "/react-projects/static/js/main.714a3a78.chunk.js"
+  },
+  {
+    "revision": "777c6568b85758f9b2a3",
+    "url": "/react-projects/static/js/runtime-main.6e0519e7.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-projects/static/media/burger-logo.b8503d26.png"
+  }
+]);
