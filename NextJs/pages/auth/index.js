5f15c9ab91fccc2a0457ada71d9ecb70189abd26
Vinicius Cechin <vinicechin@gmail.com>
2020-07-23 11:34:54 +0200

    Add special lifecycle hook async and sync as example

--- NextJs/pages/auth/index.js
+++ NextJs/pages/auth/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
 
 import User from '../../components/User'
+import { loadGetInitialProps } from 'next/dist/next-server/lib/utils'
 
-const AuthIndexPage = () => {
+const AuthIndexPage = (props) => {
     return (
         <div>
-            <h1>The auth index page</h1>
+            <h1>The auth index page - {props.appName}</h1>
             <User name="Vini" age={31} />
         </div>
     )
+}
+
+AuthIndexPage.getInitialProps = (context) => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ appName: "Server App (at Auth)" })
+        }, 1000)
+    })
+    return promise
 }
  
 export default AuthIndexPage
--- NextJs/pages/index.js
+++ NextJs/pages/index.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Link from 'next/link'
 import Router from 'next/Router'
 
-const IndexPage = () => {
-    return (
-        <div>
-            <h1>The main page</h1>
-            <p>Go to <Link href="/auth" ><a>Auth</a></Link></p>
-            <button onClick={() => Router.push('/auth')} >Go to Auth</button>
-        </div>
-    )
+class IndexPage extends Component {
+    static async getInitialProps(context) {
+        // console.log(context)
+        return {appName: 'Server App'}
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>The main page of {this.props.appName}</h1>
+                <p>Go to <Link href="/auth" ><a>Auth</a></Link></p>
+                <button onClick={() => Router.push('/auth')} >Go to Auth</button>
+            </div>
+        )
+    }
 }
  
 export default IndexPage
