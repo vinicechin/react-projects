7b9c77c64ed62b04cbec317716fc3057890b3aa9
Vinicius Cechin <vinicechin@gmail.com>
2020-07-23 10:53:39 +0200

    Add basic Next project config

---
+++ NextJs/pages/auth/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthIndexPage = () => {
+    return (
+        <div>The auth index page</div>
+    )
+}
+ 
+export default AuthIndexPage
---
+++ NextJs/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IndexPage = () => {
+    return (
+        <div>The main page</div>
+    )
+}
+ 
+export default IndexPage
