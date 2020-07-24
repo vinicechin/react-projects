f9516838237b56dcc3cdcea75acc103949a35780
Vinicius Cechin <vinicechin@gmail.com>
2020-07-24 12:57:25 +0200

    Set timings for animations

--- Animations/src/components/Modal/Modal.js
+++ Animations/src/components/Modal/Modal.js
@@ -5,9 +5,14 @@ import * as TransitionStatus from 'react-transition-group/Transition'
 
 import './Modal.css';
 
+const animationTiming = {
+    enter: 400,
+    exit: 1000
+}
+
 const modal = (props) => {
     return (
-        <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+        <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
             {state => {
                 const modalClasses = classNames({
                     'Modal': true,
