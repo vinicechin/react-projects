e0ad62c3ad87795e9efcb920441fa1fbe8749bf6
Vinicius Cechin <vinicechin@gmail.com>
2020-08-12 10:21:33 +0200

    Pass state data to other components

--- ReactHooks/src/components/Ingredients/Ingredients.js
+++ ReactHooks/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList'
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ingredients, setIngredients] = useState([])
+
+  const addIngredient = ingredient => {
+    setIngredients(prevIngredients => [
+      ...prevIngredients,
+      {
+        id: Math.random().toString(),
+        ...ingredient
+      }
+    ])
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredient} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={ingredients} onRemoveItem={() => {}} />
       </section>
     </div>
   );
